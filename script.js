3c8aad1838ac1715c88e3300fd3362d83ca993b0
diegopedroso15 <diego.pedroso15@gmail.com>
2022-01-15 20:37:48 -0300

    :packege: Novas Funcionalidades

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector(".hamburguer").addEventListener("click", function(){
+document.querySelector(".barralatera").classList.toggle("mostrar");
+document.querySelector(".container").classList.toggle("amostrar");
+});
